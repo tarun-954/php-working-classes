1d0cedd31345a6a63fc25a3da342b24e0c0a8b02
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-08-20 09:33:22 +0530

    first commit

---
+++ class1.php
@@ -0,0 +1,59 @@
+<?php
+// floating point 
+$a1=2.34;
+$a2=4.28;
+
+$a3=$a1+$a2;
+echo "Addition: $a3 <br>" ;
+
+
+$a4=$a1-$a2;
+echo " subtration:  $a4 <br>";
+
+$java="hello company";
+echo "$java";
+
+$dec1=34;
+$oct1=0243;
+$hex1=0X45;
+
+echo "decimal no: $dec1 <br>";
+echo "Octal no: $oct1 <br>";
+echo "hexadecimal no: $hex1 <br>";
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ class2.php
@@ -0,0 +1,22 @@
+<!-- creating an array in php -->
+ <?php
+$a=array("mohan","ram");
+echo gettype($a);
+?>
+
+
+<!-- Undefined variable -->
+<?php
+$b;
+echo gettype($b);
+?>
+
+<!--  if in php -->
+
+<?php
+if(TRUE)
+echo  "The cond is true";
+if(FALSE)
+echo "This false condition";
+?>
+
