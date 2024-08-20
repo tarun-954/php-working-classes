3cd1157b5218b34b6bec0b34fc2a6e4a0cd97d40
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-08-20 09:36:02 +0530

    second commit

--- class2.php
+++ class2.php
@@ -1,5 +1,5 @@
 <!-- creating an array in php -->
- <?php
+<?php
 $a=array("mohan","ram");
 echo gettype($a);
 ?>
@@ -20,3 +20,12 @@ if(FALSE)
 echo "This false condition";
 ?>
 
+
+<!-- counting the words in php -->
+<?php
+$c="welcome to php class,hello everyone ";
+echo str_word_count($c);
+?>
+
+
+<!--  -->
