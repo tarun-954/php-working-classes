6b3c33e5873800fa085a9902169c11eae41d458f
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-08-21 15:51:07 +0530

    class3

--- class3.php
+++ class3.php
@@ -8,5 +8,18 @@ echo "<br>";
 var_dump($x===$z);
 echo "<br>";
 
+// concatinate 
 echo("Hello tarun"."This is php class");
+echo "<br>";
+
+// concatenate addignment
+$str1="tarun";
+$str2="choudhary";
+echo($str1.=$str2);
+echo "<br>";
+
+echo $str1;
+
+
+
 ?>
