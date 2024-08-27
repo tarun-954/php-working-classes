7a3210bf669914ad09119b8a06a98ed4a4bf60ea
Tarun choudhary <115392420+tarun-954@users.noreply.github.com>
2024-08-27 10:31:26 +0530

    class4

--- class4.php
+++ class4.php
@@ -5,16 +5,55 @@ while($i<5){
     $i++;
 
 }
+echo "<br>";
+
+echo "-------------------------------------------------------------------------------";
+echo "<br>";
 
 //ternery operter in php
 $age=54;
 $i=($age<=18)? "child" : "adult" ;
 echo $i;
 echo "<br>";
-
+echo "-------------------------------------------------------------------------------";
+echo "<br>";
 
 //null colicing 
 $username=null;
 $i=$username ?? "guest";
 echo $i;
+echo "<br>";
+echo "-------------------------------------------------------------------------------";
+echo "<br>";
+
+//for each in php
+$arr=array("tarun","choudhary","Himachal","pradesh");
+foreach($arr as $val){
+    echo $val;
+    echo "<br>";
+}
+
+echo "<br>";
+echo "-------------------------------------------------------------------------------";
+echo "<br>";
+
+
+
+//key and value in for each loop from an array 
+
+$lpu = array("name" => "abc", "email" => "abc@gmail.com", "age" => 21);
+
+foreach ($lpu as $key => $value) {
+    // echo $key." : " ;
+    
+    // echo $value . "<br>";
+    echo ($key ." : ". $value  ."<br>");        
+}
+
+echo "<br>";
+echo "-------------------------------------------------------------------------------";
+echo "<br>";
+
+
 ?>
+
